59ffdb1cc9bb7af25fe375c8b66de1faa2372f60
Pablo Marambio <yo@pablomarambio.cl>
2013-10-28 19:26:44 -0300

    Formatting

--- lib/rut.rb
+++ lib/rut.rb
@@ -2,18 +2,35 @@
 require "rut/version"
 
 class Rut
-  def self.dv rut
-    if rut.is_a? String
-      raise ArgumentError, "El rut #{rut} debe ser un número" unless rut =~ /^\d{1,8}$/
+  class << self
+    def dv rut
+      raise ArgumentError, "El rut es nulo" if rut.nil?
+      if rut.is_a? String
+        raise ArgumentError, "El rut #{rut} debe ser un número" unless rut =~ /^\d{1,8}$/
+      end
+      rut = rut.to_s
+      x=9
+      t=0
+      rut.reverse.split(//).each do |d|
+        t+=d.to_i*x
+        x=(x==4) ? 9 : x - 1
+      end
+      r= t % 11
+      (r==10) ? "k" : r
     end
-    rut = rut.to_s
-    x=9
-    t=0
-    rut.reverse.split(//).each do |d|
-      t+=d.to_i*x
-      x=(x==4) ? 9 : x - 1
+
+    def pretty rut
+      if rut.is_a? String
+        rut.gsub! /[.\-\ ]/, ""
+        raise ArgumentError, "El rut está vacío" unless rut.length > 0
+        raise ArgumentError, "El rut '#{rut}' contiene caracteres inválidos" unless rut =~ /^\d{1,8}[\dkK]$/
+      end
+      rut = rut.to_s
+      thisdv = rut[-1, 1]
+      rut = rut[0, rut.length - 1]
+      raise ArgumentError, "El rut #{rut}-#{thisdv} es inválido" unless dv(rut).to_s.upcase == thisdv.upcase
+      rut = rut.reverse.gsub(/.{3}/, '\0.').gsub(/\.$/, '').reverse
+      "#{rut}-#{thisdv}"
     end
-    r= t % 11
-    (r==10) ? "k" : r
   end
 end
